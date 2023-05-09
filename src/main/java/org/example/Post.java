29e0bbff206fe2b06cff724f7781564110595b03
Kirill Markov <isikir668@gmail.com>
2023-05-09 12:52:52 +0300

    Task 1.

---
+++ src/main/java/org/example/Post.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Post {
+    public String name; // поле для имени
+    public String passport; // поле для паспортных данных
+    public String patronymic; // поле для отчества
+    public String phone; // поле для телефона
+    public String surname; // поле для фамилии
+    public Boolean subscription; // наличие подписки
+    public FormDate birthday;
+
+}
